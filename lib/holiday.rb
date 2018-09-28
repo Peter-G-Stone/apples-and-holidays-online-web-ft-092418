b24bf76bce73f8a5022776a3415b6ae1eb73fc37
Peter Stone <peter.stone.online@gmail.com>
2018-09-28 19:44:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,16 +28,17 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
 
-    
-  holiday_hash[season][holiday_name] = supply_array 
+  if holiday_hash[season][holiday_name] != nil 
+    (holiday_hash[season][holiday_name] << supply_array).flatten!.uniq!
+  else 
+    holiday_hash[season][holiday_name] = supply_array 
+  end
   holiday_name
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].map do |hol, val|
-    val
-  end.flatten
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -77,7 +78,6 @@ def all_supplies_in_holidays(holiday_hash)
         puts "  #{holl.to_s.split("_").map {|w| w.capitalize}.join(" ")}: #{sup.join(", ")}"
       end 
   end 
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
